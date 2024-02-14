7fdab105756c38755502b1dfea8c4a9c4fe5111f
Andrew Oliveira <andrewhenriquef@gmail.com>
2024-02-14 18:10:55 -0300

    (+) Add reverse string algorithm

---
+++ test/strings/reverse_string_test.rb
@@ -0,0 +1,26 @@
+
+require "./src/strings/reverse_string"
+require "minitest/autorun"
+
+
+# Exercise: Reversing a String
+# Description: Write a Ruby method that takes a string as an input and returns the reversed version of the string.
+
+# Inputs:
+
+# "Hello, World!"
+# "Ruby Programming"
+# "12345"
+# Expected Outputs:
+
+# "!dlroW ,olleH"
+# "gnimmargorP ybuR"
+# "54321"
+
+class StringReversalTest < Minitest::Test
+  def test_reverse_string
+    assert_equal "!dlroW ,olleH", reverse_string("Hello, World!")
+    assert_equal "gnimmargorP ybuR", reverse_string("Ruby Programming")
+    assert_equal "54321", reverse_string("12345")
+  end
+end
