35bfb5652b44cb55705416782ba58904f511ac2e
Andrew Oliveira <andrewhenriquef@gmail.com>
2024-02-27 22:07:26 -0300

    (=) improvement to  best travel distance algorithm

--- src/calculations/best_travel_distance.rb
+++ src/calculations/best_travel_distance.rb
@@ -1,7 +1,9 @@
 def choose_best_sum(max_sum_distance, number_of_towns_to_visit, distances)
   possible_combinations = distances.combination(number_of_towns_to_visit).map(&:sum)
 
-  possible_combinations.select do |distance|
+  combinations_within_max_distance = possible_combinations.select do |distance|
     distance <= max_sum_distance
-  end.max
+  end
+
+  combinations_within_max_distance.max
 end
